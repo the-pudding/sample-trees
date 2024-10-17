8e55a987aed4933061c498435f6610f35a62d7c7
Matt Daniels <matt@polygraph.cool>
2024-10-17 12:09:34 -0400

    all

--- google.config.js
+++ google.config.js
@@ -4,8 +4,18 @@ export default [
 		"filepath": "src/data/copy.json"
 	},
 	{
-		"id": "1te65h_nywgXVAzvV-9E5fmHGblcKvctf_cNZgeam9Tk",
+		"id": "1_mo6m8ue4KuNpuDrQLV5-r-jCc4gpyEJD1Te8_Hsffw",
 		"gid": "0",
-		"filepath": "src/data/test.csv"
+		"filepath": "src/data/samples.csv"
+	},
+	{
+		"id": "1_mo6m8ue4KuNpuDrQLV5-r-jCc4gpyEJD1Te8_Hsffw",
+		"gid": "443640710",
+		"filepath": "src/data/links.csv"
+	},
+	{
+		"id": "1_mo6m8ue4KuNpuDrQLV5-r-jCc4gpyEJD1Te8_Hsffw",
+		"gid": "116287331",
+		"filepath": "src/data/nodes.csv"
 	}
 ]
